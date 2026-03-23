7ccf461c6d76c266748ff1f3bc82e672504a9abc
Mateus Nascimento <mw_nascimento@outlook.com>
2026-03-23 17:56:05 -0300

    test: change gtm strategy

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -34,7 +34,7 @@ export default function RootLayout({
         {gtmId && (
           <Script
             id="gtm-script"
-            strategy="afterInteractive"
+            strategy="beforeInteractive"
             dangerouslySetInnerHTML={{
               __html: `
                 (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
